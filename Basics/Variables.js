d3b7aa84b13c499de40ed6f7d457e08466ee5fc2
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-15 12:43:26 +0000

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,19 @@
+const accountId = 151213
+let accountEmail = "Aleph123@gmail.com"
+var accountPassword ="aLePh@123"
+accountCity = "Allahabad"
+
+// accountId = 12 // const are ummutable, can't be change.
+accountEmail = "aleph.node@gmail.com"
+accountPassword = "aldA@123"
+accountCity = "Prayagraj" // Not a correct way
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
