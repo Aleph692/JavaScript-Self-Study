0688b2e14bdc3d00c93692d7b293cf6cedfd70f1
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-16 18:09:52 +0000

    Datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // Treat all JS code in new version
+// alert("Error") // This work in Browser, Not in Node Js.
+
+console.log("Code should be readable")
+
+// console.log(2 +
+//     3);  This is not good pratices
+
+//   https://tc39.es/ecma262/ --------> use for documataion
+
+console.log("Aleph");
+
+let name = "Aleph"
+let age = 25
+let isLoggedIn = false
+
+// primiative
+
+// Number ==> 2 to the power of 53
+// Null ==> not value is present  
+// String ==> ""
+// Symbol ==> unique 
+// BigInt ==> Use to store very big number
+// Boolean ==> True / false
+// undefined ==> Value is not defined
+
+//  Non primiative
+
+// Array
+// Object
+// Function
+
+
+console.log(typeof null); // Type of null is object
+console.log(typeof undefined); 
