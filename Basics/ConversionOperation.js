0e1a1e5cbe367ea4557992365b1f30eb9aaf5304
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-16 21:49:22 +0000

    Conversion_of_Datatypes

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "33aac"
+// let score = null
+let score = true
+
+console.log(score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+// "34" ==> 34
+// "34adn" ==> NaN
+// null ==> 0
+// true ==> 1 and false ==>0
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
