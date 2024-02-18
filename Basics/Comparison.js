08e069199f46e07133cd42f5110d321fb98f1d5b
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-18 01:08:29 +0000

    Comparison

---
+++ Basics/Comparison.js
@@ -0,0 +1,24 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// Different Datatyper Comparison
+
+// console.log(5<"5");  // This will change the string into a number. 
+// console.log("5"<6); 
+// console.log(null < 0); 
+// console.log(null == 0); 
+// console.log(null >= 0); 
+
+// console.log(undefined < 0); 
+// console.log(undefined = 0); 
+// console.log(undefined >= 0); 
+
+// Use Srticit (===)
+// console.log(5==="5"); // This will check the Datatyps also. (Only allow same datatypes)
+
+console.log(null === 0); 
