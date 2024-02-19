4cf02a6e9c6c3efe3e9f29abec2b1e2f56316942
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-19 04:10:56 +0000

    Maths

---
+++ Basics/Maths.js
@@ -0,0 +1,20 @@
+// const score = 100
+// console.log(score);
+
+// // const accountNumber = new Number(13041997123)
+// // console.log(accountNumber);
+// // console.log(accountNumber.toString().length);
+// // console.log(score.toFixed(2));
+// // console.log(score);
+//  const floatNumber = new Number(4.12343184)
+//  console.log(floatNumber.toPrecision(3));
+
+//  const numRepr = 1000000000000
+//  console.log(numRepr.toLocaleString("en-IN")); // output is 10,00,00,00,00,000
+
+// ____________________________________________Maths___________________________________________
+
+console.log(Math);
+console.log(Math.random()+1);
+console.log(Math.floor(Math.random()*100));
+
