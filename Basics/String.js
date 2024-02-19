b4bea221026e008950f059428f879aa4252a19b1
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-19 03:13:17 +0000

    String

---
+++ Basics/String.js
@@ -0,0 +1,12 @@
+const name = "Aleph"
+const college = "Noida International University"
+
+console.log(`I am ${name}. I a have completed my bachelor degree from ${college.bold}. `); //string interpolation.
+console.log(`I am ${name.toUpperCase()}. I a have completed my bachelor degree from ${college.toLowerCase()}.`); //string interpolation.
+ let boyGame = new String ("Mario")
+ let girlGame = "   Makeover  "
+
+ console.log(boyGame.slice(0 , 3));
+ console.log(`${girlGame.trim()} and ${boyGame}End`);
+ console.log(`${girlGame.trimEnd()} and ${boyGame}End`);
+ console.log(`${girlGame.trimStart()} and ${boyGame} End`);
