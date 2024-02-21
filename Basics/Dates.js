ede2cb52d6f164f9a561e4b4365cbbfd600affe8
Aleph <60984485+Aleph692@users.noreply.github.com>
2024-02-21 14:23:00 +0000

    Date

---
+++ Basics/Dates.js
@@ -0,0 +1,11 @@
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString);
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(typeof myDate); // type of Date is Object
+
+// let createMyDate = new Date("04-13-1997");
+// console.log(createMyDate.toLocaleDateString());
